fba2fc8ce6df0cf1a7a09d092f8982a04443df80
mohamedosama30 <mohamedosama10013@gmail.com>
2025-10-03 10:09:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-// const inputBox =document.getElementById('input-box');
-// const listContanir =document.getElementById('list-contanir');
+const inputBox =document.getElementById('input-box');
+const listContanir =document.getElementById('list-contanir');
 
-// function addtask(){  
-//     if(inputBox.value === ''){
-//         alert("You Shoud write something!")
+function addtask(){  
+    if(inputBox.value === ''){
+        alert("You Shoud write something!")
       
-//     }else{
-//         let li = document.createElement("li");
-//         // this line it's men bot (li) in inputBox(input)
-//         li.innerHTML = inputBox.value; 
-//         listContanir.appendChild(li);
-//             inputBox.value = '';
-//             let span = document.createElement("span");
-//             span.innerHTML = "\u00d7";
-//             li.appendChild(span);
+    }else{
+        let li = document.createElement("li");
+        // this line it's men bot (li) in inputBox(input)
+        li.innerHTML = inputBox.value; 
+        listContanir.appendChild(li);
+            inputBox.value = '';
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span);
 
-//     }
-// }
+    }
+}
 
-// listContanir.addEventListener("click" , function(e){
-//     if(e.target.tagName === "LI"){
-//         e.target.classList.toggle("checked")
-//     }
-//     else if(e.target.tagName === "SPAN"){
-//         e.target.parentElement.remove();
-//     }
-// }, false);
+listContanir.addEventListener("click" , function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked")
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
